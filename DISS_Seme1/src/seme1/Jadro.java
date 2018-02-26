17408dcd930af6cfdbbbe24f3b3b2d34f1ccf2b2
skazo <32869850+skazo@users.noreply.github.com>
2018-02-26 23:29:29 +0100

    nove triedy + metody

--- DISS_Seme1/src/seme1/Jadro.java
+++ DISS_Seme1/src/seme1/Jadro.java
@@ -5,11 +5,60 @@ package seme1;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Michal
  */
 public class Jadro {
+
+    private int pocetReplikacii;
+    private int pocetDveri;
+    private double pravdepodobnostA;
+    private double pravdepodobnostB;
+    private double cena;
     
+    public Jadro(){
+        
+    }
+
+    public double getCena() {
+        return cena;
+    }
+
+    public void setCena(double cena) {
+        this.cena = cena;
+    }
+
+    public double getPravdepodobnost1() {
+        return pravdepodobnostA;
+    }
+
+    public void setPravdepodobnost1(double pravdepodobnostA) {
+        this.pravdepodobnostA = pravdepodobnostA;
+    }
+
+    public double getPravdepodobnostB() {
+        return pravdepodobnostB;
+    }
+
+    public void setPravdepodobnost2(double pravdepodobnostB) {
+        this.pravdepodobnostB = pravdepodobnostB;
+    }
+
+    public int getPocetDveri() {
+        return pocetDveri;
+    }
+
+    public void setPocetDveri(int pocetDveri) {
+        this.pocetDveri = pocetDveri;
+    }
+
+    public int getPocetReplikacii() {
+        return pocetReplikacii;
+    }
+
+    public void setPocetReplikacii(int pocetReplikacii) {
+        this.pocetReplikacii = pocetReplikacii;
+    }
+
 }
