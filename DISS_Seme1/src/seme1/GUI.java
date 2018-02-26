a1c905e8d7854260f623e681f7553a29e162572b
skazo <32869850+skazo@users.noreply.github.com>
2018-02-26 15:14:56 +0100

    Pridana sprava

--- DISS_Seme1/src/seme1/GUI.java
+++ DISS_Seme1/src/seme1/GUI.java
@@ -18,6 +18,7 @@ public class GUI {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Haloo pokus");
+        System.out.println("Druhy pokus");
     }
     
 }
