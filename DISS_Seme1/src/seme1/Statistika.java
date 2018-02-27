dba5b9e8660a4283f77d3b14eb0bea0f5811c9a8
skazo <32869850+skazo@users.noreply.github.com>
2018-02-27 23:12:15 +0100

    implementacia -> nove triedy + metody

---
+++ DISS_Seme1/src/seme1/Statistika.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seme1;
+
+/**
+ *
+ * @author Michal
+ */
+public class Statistika {
+
+    public Statistika() {
+    
+    
+    }
+    
+    public void vyratajStatistiku(){
+        
+    }
+    
+    
+}
